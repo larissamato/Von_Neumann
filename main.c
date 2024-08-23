04f7e9a9553f63f36a06524952e477427aecb60f
larissamato <matoslarissa31@gmail.com>
2024-08-23 18:51:54 -0300

    :poop: fixed legacy code

--- main.c
+++ main.c
@@ -1,14 +1,49 @@
-//Terminal linux
-//gcc main.c -o a.out -lm
-//./a.out
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <stdbool.h>
 #include <string.h>
 
-//SERÁ UTILIZADO VARIÁVEIS GLOBAIS
+#define NUM_INSTRUCOES 28
+
+typedef struct {
+    const char nome[10];
+    unsigned int codigo;
+    unsigned int formato;
+} InstrucaoInfo;
+
+InstrucaoInfo instrucoes[NUM_INSTRUCOES] = {
+    {"hlt", 0x00, 0x0000},
+    {"nop", 0x01, 0x0000},
+    {"add", 0x02, 0x03},
+    {"sub", 0x03, 0x03},
+    {"mul", 0x04, 0x03},
+    {"div", 0x05, 0x03},
+    {"cmp", 0x06, 0x03},
+    {"movrr", 0x07, 0x03},
+    {"and", 0x08, 0x03},
+    {"or", 0x09, 0x03},
+    {"xor", 0x0a, 0x03},
+    {"not", 0x0b, 0x21},
+    {"je", 0x0c, 0x21},
+    {"jne", 0x0d, 0x21},
+    {"jl", 0x0e, 0x21},
+    {"jle", 0x0f, 0x21},
+    {"jg", 0x10, 0x21},
+    {"jge", 0x11, 0x21},
+    {"jmp", 0x12, 0x21},
+    {"ld", 0x13, 0x21},
+    {"st", 0x14, 0x21},
+    {"movi", 0x15, 0x21},
+    {"addi", 0x16, 0x21},
+    {"subi", 0x17, 0x21},
+    {"muli", 0x18, 0x21},
+    {"divi", 0x19, 0x21},
+    {"lsh", 0x1a, 0x21},
+    {"rsh", 0x1b, 0x21}
+};
+
+//GLOBAL VAR
 unsigned char memory [154]; //memória RAM
 unsigned int  mbr, //contém a palavra a ser armazenada na memória e recebe a palavra lida
               mar, //endereço a ser lido ou escrito da memória
@@ -66,243 +101,37 @@ void guardarMemoria(unsigned int mar2 ,unsigned int palavra){
     memory [mar2]=palavra & 0xff;
 }
 
-void Instrucao (int mar2){
-    char *token1, Tro0;
-    unsigned int count=0, endImm=0, tipo;
-    //printf("mar:%d", mar2);
-    token1 = strtok(token," ,");
-    while(token1){
-        //printf("TOKEN INSTRUÇÃÇO: %s\n",token1);
-        if(count==0){
-            strcpy(compara,token1);
-            //printf("TOKEN COMPARA: %s\n",compara);
-        }else if(count==1){
-            Tro0=(int)strtol(token1,NULL,16);
-            strcpy(flag,token1);
-       
-        }else if(count==2){
-            endImm=(int)strtol(token1,NULL,16);
-            strcpy(flagedimm,token1);
+void Instrucao(int mar2) {
+    char *token1;
+    unsigned int count = 0, endImm = 0, tipo;
+    unsigned int palavra = 0x00;
 
+    token1 = strtok(token, " ,");
+
+    while (token1) {
+        if (count == 0) {
+            strcpy(compara, token1);
+        } else if (count == 1) {
+            palavra = (palavra << 24) | (unsigned int) strtol(token1, NULL, 16);
+        } else if (count == 2) {
+            endImm = (unsigned int) strtol(token1, NULL, 16);
         }
-            
-        token1 = strtok(NULL," ,");
+        token1 = strtok(NULL, " ,");
         count++;
     }
-    if(strcmp(compara,"hlt")== 0){
-    unsigned int palavra = 0x00;
-    palavra=(palavra<<24)|endImm;
-    guardarMemoria(mar2,palavra);
-    //printf("palavra %x", palavra);
-    } 
-    if(strcmp(compara,"nop")== 0){
-    unsigned int palavra = 0x01;
-    palavra=(palavra<<24)|endImm;
-    guardarMemoria(mar2,palavra);
-    //printf("palavra %x", palavra);
-    } 
-    if(strcmp(compara,"add")== 0){
-        int palavra = 0x02;
-        Tro0 = strtol(&flag[1], NULL, 10);
-        endImm = strtol(&flagedimm[1], NULL, 10);
-        palavra=(palavra<<3)|Tro0; 
-        palavra=(palavra<<3)|endImm;
-        palavra=(palavra<<18);
-        guardarMemoria(mar2,palavra);
-    } 
-    if(strcmp(compara,"sub")== 0){
-        int palavra = 0x03;
-        Tro0 = strtol(&flag[1], NULL, 10);
-        endImm = strtol(&flagedimm[1], NULL, 10);
-        palavra=(palavra<<3)|Tro0; 
-        palavra=(palavra<<3)|endImm;
-        palavra=(palavra<<18);
-        guardarMemoria(mar2,palavra);
-    } 
-     if(strcmp(compara,"mul")== 0){
-        int palavra = 0x04;
-        Tro0 = strtol(&flag[1], NULL, 10);
-        endImm = strtol(&flagedimm[1], NULL, 10);
-        palavra=(palavra<<3)|Tro0; 
-        palavra=(palavra<<3)|endImm;
-        palavra=(palavra<<18);
-        guardarMemoria(mar2,palavra);
+
+    for (int i = 0; i < NUM_INSTRUCOES; i++) {
+        if (strcmp(compara, instrucoes[i].nome) == 0) {
+            palavra = (instrucoes[i].codigo << 24) | ((palavra & instrucoes[i].formato) << (32 - __builtin_popcount(instrucoes[i].formato)));
+            if (instrucoes[i].formato == 0x21) {
+                palavra |= (endImm & 0x001FFFFF);
+            }
+            guardarMemoria(mar2, palavra);
+            return;
+        }
     }
-     if(strcmp(compara,"div")== 0){
-        int palavra = 0x05;
-        Tro0 = strtol(&flag[1], NULL, 10);
-        endImm = strtol(&flagedimm[1], NULL, 10);
-        palavra=(palavra<<3)|Tro0; 
-        palavra=(palavra<<3)|endImm;
-        palavra=(palavra<<18);
-        guardarMemoria(mar2,palavra);
-    }
-     if(strcmp(compara,"cmp")== 0){
-        int palavra = 0x06;
-        Tro0 = strtol(&flag[1], NULL, 10);
-        endImm = strtol(&flagedimm[1], NULL, 10);
-        palavra=(palavra<<3)|Tro0; 
-        palavra=(palavra<<3)|endImm;
-        palavra=(palavra<<18);
-        guardarMemoria(mar2,palavra);
-    }
-     if(strcmp(compara,"movrr")== 0){
-        int palavra = 0x07;
-        Tro0 = strtol(&flag[1], NULL, 10);
-        endImm = strtol(&flagedimm[1], NULL, 10);
-        palavra=(palavra<<3)|Tro0; 
-        palavra=(palavra<<3)|endImm;
-        palavra=(palavra<<18);
-        guardarMemoria(mar2,palavra);
-    }
-     if(strcmp(compara,"and")== 0){
-        int palavra = 0x08;
-        Tro0 = strtol(&flag[1], NULL, 10);
-        endImm = strtol(&flagedimm[1], NULL, 10);
-        palavra=(palavra<<3)|Tro0; 
-        palavra=(palavra<<3)|endImm;
-        palavra=(palavra<<18);
-        guardarMemoria(mar2,palavra);
-    }
-     if(strcmp(compara,"or")== 0){
-        int palavra = 0x09;
-        Tro0 = strtol(&flag[1], NULL, 10);
-        endImm = strtol(&flagedimm[1], NULL, 10);
-        palavra=(palavra<<3)|Tro0; 
-        palavra=(palavra<<3)|endImm;
-        palavra=(palavra<<18);
-        guardarMemoria(mar2,palavra);
-    }
-     if(strcmp(compara,"xor")== 0){
-        int palavra = 0xa;
-        Tro0 = strtol(&flag[1], NULL, 10);
-        endImm = strtol(&flagedimm[1], NULL, 10);
-        palavra=(palavra<<3)|Tro0; 
-        palavra=(palavra<<3)|endImm;
-        palavra=(palavra<<18);
-        guardarMemoria(mar2,palavra);
-    }
-     if(strcmp(compara,"not")== 0){
-    unsigned int palavra = 0x0b;
-    Tro0 = strtol(&flag[1], NULL, 10);
-    palavra=(palavra<<3)|Tro0;
-    palavra=(palavra<<21)|endImm;
-    guardarMemoria(mar2,palavra);
-    }
-     if(strcmp(compara,"je")== 0){
-    unsigned int palavra = 0x0c;
-    palavra=(palavra<<3)|Tro0;
-    palavra=(palavra<<21)|endImm;
-    guardarMemoria(mar2,palavra);
-    }
-     if(strcmp(compara,"jne")== 0){
-    unsigned int palavra = 0x0d;
-    palavra=(palavra<<3)|Tro0;
-    palavra=(palavra<<21)|endImm;
-    guardarMemoria(mar2,palavra);
-    }
-     if(strcmp(compara,"jl")== 0){
-    unsigned int palavra = 0x0e;
-    palavra=(palavra<<3)|Tro0;
-    palavra=(palavra<<21)|endImm;
-    guardarMemoria(mar2,palavra);
-    }
-     if(strcmp(compara,"jle")== 0){
-    unsigned int palavra = 0x0f;
-    palavra=(palavra<<3)|Tro0;
-    palavra=(palavra<<21)|endImm;
-    guardarMemoria(mar2,palavra);
-    }
-    if(strcmp(compara,"jg")== 0){
-    unsigned int palavra = 0x10;
-    palavra=(palavra<<3)|Tro0;
-    palavra=(palavra<<21)|endImm;
-    guardarMemoria(mar2,palavra);
-    }
-    if(strcmp(compara,"jge")== 0){
-    unsigned int palavra = 0x11;
-    palavra=(palavra<<3)|Tro0;
-    palavra=(palavra<<21)|endImm;
-    guardarMemoria(mar2,palavra);
-    }
-    if(strcmp(compara,"jmp")== 0){
-    unsigned int palavra = 0x12;
-    palavra=(palavra<<3)|Tro0;
-    palavra=(palavra<<21)|endImm;
-    guardarMemoria(mar2,palavra);
-    }
-    if(strcmp(compara,"ld")== 0){
-        unsigned int palavra = 0x13;
-        Tro0 = strtol(&flag[1], NULL, 10);
-        palavra=(palavra<<3)|Tro0;
-        palavra=(palavra<<21)|endImm;
-        guardarMemoria(mar2,palavra);
-        //printf("\t\t\t\tpalavra %x\n\n", palavra);
-    }
-     if(strcmp(compara,"st")== 0){
-        unsigned int palavra = 0x14;
-        Tro0 = strtol(&flag[1], NULL, 10);
-        palavra=(palavra<<3)|Tro0;
-        palavra=(palavra<<21)|endImm;
-        guardarMemoria(mar2,palavra);
-        //printf("\t\t\t\tpalavra %x\n\n", palavra);
-    }  
-    if(strcmp(compara,"movi")== 0){
-        unsigned int palavra = 0x15;
-        Tro0 = strtol(&flag[1], NULL, 10);
-        palavra=(palavra<<3)|Tro0;
-        palavra=(palavra<<21)|endImm;
-        guardarMemoria(mar2,palavra);
-        //printf("\t\t\t\tpalavra %x\n\n", palavra);
-    } 
-    if(strcmp(compara,"addi")== 0){
-        unsigned int palavra = 0x16;
-        Tro0 = strtol(&flag[1], NULL, 10);
-        palavra=(palavra<<3)|Tro0;
-        palavra=(palavra<<21)|endImm;
-        guardarMemoria(mar2,palavra);
-        //printf("\t\t\t\tpalavra %x\n\n", palavra);
-    }   
-     if(strcmp(compara,"subi")== 0){
-        unsigned int palavra = 0x17;
-        Tro0 = strtol(&flag[1], NULL, 10);
-        palavra=(palavra<<3)|Tro0;
-        palavra=(palavra<<21)|endImm;
-        guardarMemoria(mar2,palavra);
-        //printf("\t\t\t\tpalavra %x\n\n", palavra);
-    }  
-    if(strcmp(compara,"muli")== 0){
-        unsigned int palavra = 0x18;
-        Tro0 = strtol(&flag[1], NULL, 10);
-        palavra=(palavra<<3)|Tro0;
-        palavra=(palavra<<21)|endImm;
-        guardarMemoria(mar2,palavra);
-        //printf("\t\t\t\tpalavra %x\n\n", palavra);
-    }
-    if(strcmp(compara,"divi")== 0){
-        unsigned int palavra = 0x19;
-        Tro0 = strtol(&flag[1], NULL, 10);
-        palavra=(palavra<<3)|Tro0;
-        palavra=(palavra<<21)|endImm;
-        guardarMemoria(mar2,palavra);
-        //printf("\t\t\t\tpalavra %x\n\n", palavra);
-    }  
-    if(strcmp(compara,"lsh")== 0){
-        unsigned int palavra = 0x1a;
-        Tro0 = strtol(&flag[1], NULL, 10);
-        palavra=(palavra<<3)|Tro0;
-        palavra=(palavra<<21)|endImm;
-        guardarMemoria(mar2,palavra);
-        //printf("\t\t\t\tpalavra %x\n\n", palavra);
-    }  
-     if(strcmp(compara,"rsh")== 0){
-        unsigned int palavra = 0x1b;
-        Tro0 = strtol(&flag[1], NULL, 10);
-        palavra=(palavra<<3)|Tro0;
-        palavra=(palavra<<21)|endImm;
-        guardarMemoria(mar2,palavra);
-        //printf("\t\t\t\tpalavra %x\n\n", palavra);
-    }          
+
+    printf("Instrução desconhecida: %s\n", compara);
 }
 
 void Entrada(){
@@ -311,7 +140,6 @@ void Entrada(){
     unsigned char tipo,valor;
 
 
-    // ABRE O ARQUIVO 
     arquivo = fopen("teste.txt", "rt");
     if (arquivo == NULL) {
         printf("Problemas na abertura do arquivo\n");
@@ -319,20 +147,15 @@ void Entrada(){
     }
     
 
-    // PERCORRE ARQUIVO
     while (!feof(arquivo)){ 
         token = fgets(str, 100, arquivo);  
         token = strtok(str, s);
-        //printf("Linha %d : %s\n",i,token);
 
         while(token) {
-            //printf("token: %s\n",token);
             if(contador==0){
                 mar2 = (int)strtol(token,NULL,16);
-                //printf("Print endereço:%i ---- %x\n", mar2,mar2);
             }else if (contador==1){
                 tipopalavra = (*token=='i') ? 1:0;
-                //printf("Print tipo:%i ---- %x\n", tipopalavra,tipopalavra);
             }
             else if(contador==2){
                 if(tipopalavra==0){
@@ -341,7 +164,6 @@ void Entrada(){
                     memory [mar2++]=(inteiro>>16) & 0xff;    
                     memory [mar2++]=(inteiro>>8) & 0xff;
                     memory [mar2]=inteiro & 0xff;
-                    //printf("Print dado guardado:%x\n", inteiro);
                 }else{
                 Instrucao(mar2);
                 }
@@ -408,236 +230,100 @@ void Decodifica(){
     }
     Print();
 }
-void Executa (){
+
+void executeArithmeticOperation(uint op) {
+    switch (op) {
+        case 0x02: reg[ro0] += reg[ro1]; break; // add
+        case 0x03: reg[ro0] -= reg[ro1]; break; // sub
+        case 0x04: reg[ro0] *= reg[ro1]; break; // mul
+        case 0x05: reg[ro0] /= reg[ro1]; break; // div
+        case 0x08: reg[ro0] &= reg[ro1]; break; // and
+        case 0x09: reg[ro0] |= reg[ro1]; break; // or
+        case 0x0a: reg[ro0] ^= reg[ro1]; break; // xor
+        case 0x0b: reg[ro0] = !reg[ro0]; break; // not
+        case 0x15: reg[ro0] = imm; break;       // movi
+        case 0x16: reg[ro0] += imm; break;       // addi
+        case 0x17: reg[ro0] -= imm; break;       // subi
+        case 0x18: reg[ro0] *= imm; break;       // muli
+        case 0x19: reg[ro0] /= imm; break;       // divi
+        case 0x1a: reg[ro0] <<= imm; break;      // lsh
+        case 0x1b: reg[ro0] >>= imm; break;      // rsh
+        default: break;
+    }
+}
+
+void executeComparison(uint op) {
+    switch (op) {
+        case 0x06:
+            if (reg[ro0] == reg[ro1]) { e = 1; l = g = 0; }
+            else if (reg[ro0] < reg[ro1]) { l = 1; g = e = 0; }
+            else { g = 1; l = e = 0; }
+            break;
+        case 0x0c: if (e) pc = mar; else pc += 4; break; // je
+        case 0x0d: if (!e) pc = mar; else pc += 4; break; // jne
+        case 0x0e: if (l) pc = mar; else pc += 4; break; // jl
+        case 0x0f: if (e || l) pc = mar; else pc += 4; break; // jle
+        case 0x10: if (g) pc = mar; else pc += 4; break; // jg
+        case 0x11: if (e || g) pc = mar; else pc += 4; break; // jge
+        case 0x12: pc = mar; break; // jmp
+        default: break;
+    }
+}
+
+void executeMemoryOperation(uint op) {
+    switch (op) {
+        case 0x13:
+            if (!flagrepeticao) {
+                mbr = (memory[mar++] << 24) | (memory[mar++] << 16) | (memory[mar++] << 8) | memory[mar++];
+                reg[ro0] = mbr;
+                flagrepeticao = true;
+            } else {
+                mbr = (memory[mar++] << 24) | (memory[mar++] << 16) | (memory[mar++] << 8) | memory[mar++];
+                reg[ro1] = mbr;
+                flagrepeticao = false;
+            }
+            pc += 4;
+            break;
+        case 0x14:
+            mbr = reg[ro0];
+            memory[mar++] = (mbr >> 24) & 0xff;
+            memory[mar++] = (mbr >> 16) & 0xff;
+            memory[mar++] = (mbr >> 8) & 0xff;
+            memory[mar] = mbr & 0xff;
+            pc += 4;
+            break;
+        default: break;
+    }
+}
+
+void Executa() {
     Clique();
-    //add
-     if(ir==0x02){
-        reg[ro0]=reg[ro1]+reg[ro0];
-        pc +=4;}
-    //sub
-    else if(ir==0x03){
-        reg[ro0]=reg[ro0]-reg[ro1];
-        pc +=4;}
-    //mul
-    else if(ir==0x04){
-        reg[ro0]=reg[ro0]*reg[ro1];
-        pc +=4;}
-    //div
-    else if(ir==0x05){
-        reg[ro0]=reg[ro0]/reg[ro1];
-        pc +=4;}
-    //cmp
-    else if(ir==0x06){
-       if(reg[ro0]==reg[ro1]){
-           e=1;
-           pc+=4;
-       }else if(reg[ro0]<reg[ro1]){
-           l=1;
-           g=0;
-           e=0;
-           pc+=4;
-       }else if(reg[ro0]>reg[ro1]){
-           l=0;
-           g=1;
-           e=0;
-           pc+=4;
-       }
+
+    switch (ir) {
+        case 0x02: case 0x03: case 0x04: case 0x05: case 0x08: case 0x09: case 0x0a: case 0x0b: case 0x15: case 0x16: case 0x17: case 0x18: case 0x19: case 0x1a: case 0x1b:
+            executeArithmeticOperation(ir);
+            pc += 4;
+            break;
+        case 0x06: case 0x0c: case 0x0d: case 0x0e: case 0x0f: case 0x10: case 0x11: case 0x12:
+            executeComparison(ir);
+            break;
+        case 0x13: case 0x14:
+            executeMemoryOperation(ir);
+            break;
+        default: break;
     }
-    //movr
-    else if(ir==0x07){
-        reg[ro0]=reg[ro1];
-        pc +=4;}
-    //and
-    else if(ir==0x08){
-        reg[ro0]=reg[ro0]&reg[ro1];
-        pc +=4;}
-    //or
-    else if(ir==0x09){
-        reg[ro0]=reg[ro0]|reg[ro1];
-        pc +=4;}
-    //xor
-    else if(ir==0x0a){
-        reg[ro0]=reg[ro0]^reg[ro1];
-        pc +=4;}
-    //not
-    else if(ir==0x0b){
-        reg[ro0]=!reg[ro0];
-        pc +=4;}
-    //je
-    else if(ir==0x0c){   
-        if(e==1){
-        pc=mar;
-        }else {
-        pc+=4;
-        }
-    }
-    //jne
-    else if(ir==0x0d){
-       if(e==0){
-        pc=mar;
-        }else{
-        pc+=4;
-        }
-    }
-    //jl
-    else if(ir==0x0e){
-        if(l==1){
-        pc=mar;
-        }else{
-        pc+=4;
-        }
-    }
-    //jle
-    else if(ir==0x0f){
-        if(e==1 || l==1){
-        pc=mar;
-        }else{
-        pc+=4;
-        }
-    }
-    //jg
-    else if(ir==0x10){
-        if(g==1){
-        pc=mar;
-        }else{
-        pc+=4;
-        }
-    }
-    //jge
-    else if(ir==0x11){
-        if(e==1 || g==1){
-        pc=mar;
-        }else{
-        pc+=4;
-        }
-    }
-    //jmp
-    else if(ir==0x12){
-        pc=mar;
-    }
-    //load
-    else if(ir==0x13){
-        if(flagrepeticao==false)
-        {
-            mbr= memory[mar++] << 8;             
-            mbr= (mbr | memory [mar++]) << 8;    
-            mbr= (mbr | memory [mar++]) << 8;
-            mbr= memory [mar++];
-            reg[ro0]=mbr;
-            flagrepeticao=true;
-            pc +=4;
-        }
-        else if(flagrepeticao==true)
-        {
-            mbr= memory[mar++] << 8;             
-            mbr= (mbr | memory [mar++]) << 8;    
-            mbr= (mbr | memory [mar++]) << 8;
-            mbr= memory [mar++];
-            reg[ro1]=mbr; 
-            pc +=4;
-        }
-    }
-    //store
-    else if(ir==0x14){
-    mbr=reg[ro0];
-    memory [mar++]=(mbr>>24) & 0xff; 
-    memory [mar++]=(mbr>>16) & 0xff;    
-    memory [mar++]=(mbr>>8) & 0xff;
-    memory [mar]=mbr & 0xff;
-    pc+=4;
-    
-    }
-    //movi
-     else if(ir==0x15){
-        reg[ro0]=imm;
-        pc +=4;
-    }  
-    //addi
-    else if(ir==0x16){
-        reg[ro0]=reg[ro0]+imm;
-        pc +=4;
-    } 
-    //subi
-    else if(ir==0x17){
-        reg[ro0]=reg[ro0]-imm;
-        pc +=4;
-    } 
-    //muli
-    else if(ir==0x18){
-        reg[ro0]=imm*reg[ro0];
-        pc +=4;
-    } 
-    //divi
-    else if(ir==0x19){
-        reg[ro0]=reg[ro0]/imm;
-        pc +=4;
-    }
-    //lsh
-    else if(ir==0x1a){   
-        reg[ro0]=reg[ro0]<<imm;
-        pc +=4;
-    }
-    //rsh
-    else if(ir==0x1b){   
-        reg[ro0]=reg[ro0]>>imm;
-        pc +=4;
-    }
+
     Print();
 }
 
-
 int main (){
-/*ALOCAR MEMÓRIA PARA TESTES
-memory [0]= 0x13;
-memory [1]= 0x00;
-memory [2]= 0x00;
-memory [3]= 0x1e;
-memory [4]= 0x13;
-memory [5]= 0x20;
-memory [6]= 0x0;
-memory [7]= 0x24;
-memory [8]= 0x2;
-memory [9]= 0x4;
-memory [10]= 0x0;
-memory [11]= 0x0;
-memory [12]= 0x16;
-memory [13]= 0x0;
-memory [14]= 0x0;
-memory [15]= 0x14;
-memory [16]= 0x14;
-memory [17]= 0x0;
-memory [18]= 0x0;
-memory [19]= 0x26;
-memory [20]= 0x1;
-memory [21]= 0x0;
-memory [22]= 0x0;
-memory [23]= 0x0;
-memory [24]= 0x0;
-memory [25]= 0x0;
-memory [26]= 0x0;
-memory [27]= 0x0;
-memory [28]= 0x0;
-memory [29]= 0x0;
-memory [30]= 0x0;
-memory [31]= 0x0;
-memory [32]= 0x0;
-memory [33]= 0xf;
-memory [36]= 0x0;
-memory [37]= 0x0;
-memory [38]= 0x0;
-memory [39]= 0x8;
-memory [40]= 0x0;
-memory [41]= 0x0;
-memory [42]= 0x0;
-memory [43]= 0x0;
-memory [44]= 0x0;*/
-mbr=0;
+  mbr=0;
 
-printf("mbr inicio: %x\n", mbr);
+  printf("mbr inicio: %x\n", mbr);
 
-while(flagexecucao){
-int i;
-i=i+1;
+  while(flagexecucao){
+    int i;
+    i=i+1;
 
     Entrada();
     Busca();
@@ -645,10 +331,6 @@ i=i+1;
     Executa();
 
     printf("----------------------------------------------------------------\n");
+  }
+  return 0;
 }
-return 0;
-}
-
-
-//gcc trabalho.c -o a.out -lm
-//./a.out
